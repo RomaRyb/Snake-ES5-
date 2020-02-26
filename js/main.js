9af2da1ebab004dae4ffa0a986fb37df5640c697
Roma Rub <rubashevskyiroman@gmail.com>
2020-02-26 21:28:13 +0200

    score animation added test

--- js/main.js
+++ js/main.js
@@ -7,26 +7,44 @@ var canvas = document.getElementById("canvas");
     
     //we compute the width and height in the cells
     var blockSize = 10;
+
     var widthInBlocks = width / blockSize;
     var heightInBlocks = height / blockSize;
 
-    // we set up an account
+    // count setup
     var score = 0;
 
-    /*we draw a frame*/
+    /*draw a frame*/
         var drawBorder = function () {
-        ctx.fillStyle = "Grey";
+        ctx.fillStyle = "gray";
         ctx.fillRect(0, 0, width, blockSize);
         ctx.fillRect(0, height - blockSize, width, blockSize);
         ctx.fillRect(0, 0, blockSize, height)
         ctx.fillRect(width - blockSize, 0, blockSize, height);
     };
 
-    /*display the game score in the upper left corner*/
+    /*display game score in the upper left corner*/
     var drawScore = function () {
-        ctx.font = "20px Courier";
-        ctx.fillStyle = "Black";
+        ctx.font = "15px Courier";
+        ctx.fillStyle = "gold";
         ctx.textAlign = "left";
         ctx.textBaseLine = "top";
         ctx.fillText("Your Score: " + score, blockSize, blockSize);
     };
+
+
+    var gameOver = function() {
+        //clearInterval(intervalId);
+        ctx.fillStyle = "black";
+        ctx.font = "60px Courier";
+        ctx.textAlign = "center"
+        ctx.textBaseline = "middle";    
+        ctx.fillText = ("Game Over", width / 2, height / 2)
+    };
+
+    setInterval (function() {
+        ctx.clearRect(0, 0, width, height);
+            drawBorder();
+            drawScore();
+            score++;
+    }, 100);
